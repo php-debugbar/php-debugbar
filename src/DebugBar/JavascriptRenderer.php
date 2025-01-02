bdcd0841e3be2bc7ac9326fafce8747bd869162f
Barry vd. Heuvel <barry@fruitcake.nl>
2025-01-02 14:44:00 +0100

    Add option to set theme

--- src/DebugBar/JavascriptRenderer.php
+++ src/DebugBar/JavascriptRenderer.php
@@ -62,6 +62,8 @@ class JavascriptRenderer
 
     protected $useRequireJs = false;
 
+    protected $theme = null;
+
     protected $hideEmptyTabs = null;
 
     protected $initialization;
@@ -158,6 +160,9 @@ class JavascriptRenderer
         }
         if (array_key_exists('use_requirejs', $options)) {
             $this->setUseRequireJs($options['use_requirejs']);
+        }
+        if (array_key_exists('theme', $options)) {
+            $this->setTheme($options['theme']);
         }
         if (array_key_exists('hide_empty_tabs', $options)) {
             $this->setHideEmptyTabs($options['hide_empty_tabs']);
@@ -402,6 +407,17 @@ class JavascriptRenderer
         return $this->useRequireJs;
     }
 
+    /**
+     * Sets whether to hide empty tabs or not
+     *
+     * @param boolean $hide
+     * @return $this
+     */
+    public function setTheme($theme='auto')
+    {
+        $this->theme = $theme;
+        return $this;
+    }
 
     /**
      * Sets whether to hide empty tabs or not
@@ -1131,6 +1147,10 @@ class JavascriptRenderer
         if ($this->hideEmptyTabs !== null) {
             $js .= sprintf("%s.setHideEmptyTabs(%s);\n", $this->variableName,
                 json_encode($this->hideEmptyTabs));
+        }
+
+        if ($this->theme !== null) {
+            $js .= sprintf("%s.setTheme(%s);\n", $this->variableName, json_encode($this->theme));
         }
 
         if (($this->initialization & self::INITIALIZE_CONTROLS) === self::INITIALIZE_CONTROLS) {
--- src/DebugBar/Resources/debugbar.js
+++ src/DebugBar/Resources/debugbar.js
@@ -459,6 +459,18 @@ if (typeof(PhpDebugBar) == 'undefined') {
             setTimeout(f, 20);
         },
 
+        setTheme: function(theme) {
+            if (theme === 'auto') {
+                const mediaQueryList = window.matchMedia("(prefers-color-scheme: dark)");
+                theme = mediaQueryList.matches ? 'dark' : 'light';
+                mediaQueryList.addEventListener('change', event => {
+                    this.$el.attr('data-theme', event.matches ? 'dark' : 'light')
+                })
+            }
+
+            this.$el.attr('data-theme', theme)
+        },
+
         /**
          * Resizes the debugbar to fit the current browser window
          */
