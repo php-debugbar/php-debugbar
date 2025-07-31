ad3eb782d4d5d01da74d5350576ea2fddd66fd4d
Martins Ayoola <47458749+omartins365@users.noreply.github.com>
2025-07-31 13:02:32 +0000

    Add tests for WSL link generation in HasXdebugLinks

--- tests/DebugBar/Tests/HasXdebugLinksTest.php
+++ tests/DebugBar/Tests/HasXdebugLinksTest.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace DebugBar\Tests;
+
 use PHPUnit\Framework\TestCase;
 use DebugBar\DataFormatter\HasXdebugLinks;
 
@@ -16,5 +18,29 @@ class HasXdebugLinksTest extends TestCase
         $link = $this->getXdebugLink($file, $line);
         $this->assertStringContainsString('vscode://vscode-remote/wsl+CustomWSL', $link['url']);
         $this->assertStringContainsString('index.php:42', urldecode($link['url']));
+    }
+
+    public function testVscodeWslLinkGenerationWithDefaultWslName()
+    {
+        $this->setEditorLinkTemplate('vscode-wsl');
+        // Do not set WSL name, should default to 'Ubuntu'
+        $file = '/var/www/html/test.php';
+        $line = 5;
+        $link = $this->getXdebugLink($file, $line);
+        $this->assertStringContainsString('vscode://vscode-remote/wsl+Ubuntu', $link['url']);
+        $this->assertStringContainsString('test.php:5', urldecode($link['url']));
+    }
+
+    public function testVscodeWslLinkGenerationWithEnvVar()
+    {
+        $this->setEditorLinkTemplate('vscode-wsl');
+        putenv('DEBUGBAR_WSL_NAME=EnvWSL');
+        // Do not set WSL name via setter, should use env var
+        $file = '/var/www/html/env.php';
+        $line = 7;
+        $link = $this->getXdebugLink($file, $line);
+        $this->assertStringContainsString('vscode://vscode-remote/wsl+EnvWSL', $link['url']);
+        $this->assertStringContainsString('env.php:7', urldecode($link['url']));
+        putenv('DEBUGBAR_WSL_NAME'); // Clean up
     }
 }
