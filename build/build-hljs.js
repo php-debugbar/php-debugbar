2a2107ddc088f7b23e2dc52536cddeb8a1527ab1
Barry vd. Heuvel <barry@fruitcake.nl>
2025-12-21 11:32:34 +0100

    Build javascript assets

---
+++ build/build-hljs.js
@@ -0,0 +1,23 @@
+// Build custom highlight.js bundle with specific languages
+import hljs from 'highlight.js/lib/core';
+import php from 'highlight.js/lib/languages/php';
+import phpTemplate from 'highlight.js/lib/languages/php-template';
+import javascript from 'highlight.js/lib/languages/javascript';
+import sql from 'highlight.js/lib/languages/sql';
+import shell from 'highlight.js/lib/languages/shell';
+import css from 'highlight.js/lib/languages/css';
+import plaintext from 'highlight.js/lib/languages/plaintext';
+import xml from 'highlight.js/lib/languages/xml';
+import yaml from 'highlight.js/lib/languages/yaml';
+
+hljs.registerLanguage('php', php);
+hljs.registerLanguage('php-template', phpTemplate);
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('sql', sql);
+hljs.registerLanguage('shell', shell);
+hljs.registerLanguage('css', css);
+hljs.registerLanguage('plaintext', plaintext);
+hljs.registerLanguage('xml', xml);
+hljs.registerLanguage('yaml', yaml);
+
+export default hljs;
