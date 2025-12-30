de00d1749779690d29671ff2618a6c6d254c47be
Barry vd. Heuvel <barry@fruitcake.nl>
2025-12-30 19:50:57 +0100

    Check if equal, early exit

--- resources/debugbar.js
+++ resources/debugbar.js
@@ -38,6 +38,52 @@ window.PhpDebugBar = window.PhpDebugBar || {};
     };
 
     /**
+     * Copyright (c) 2017 Evgeny Poberezkin
+     * https://github.com/epoberezkin/fast-deep-equal
+     * @type {(function(*, *): (*|boolean))|*}
+     */
+    const equal = PhpDebugBar.utils.equal = function (a, b) {
+        if (a === b) return true;
+
+        if (a && b && typeof a == 'object' && typeof b == 'object') {
+            if (a.constructor !== b.constructor) return false;
+
+            var length, i, keys;
+            if (Array.isArray(a)) {
+                length = a.length;
+                if (length != b.length) return false;
+                for (i = length; i-- !== 0;)
+                    if (!equal(a[i], b[i])) return false;
+                return true;
+            }
+
+
+
+            if (a.constructor === RegExp) return a.source === b.source && a.flags === b.flags;
+            if (a.valueOf !== Object.prototype.valueOf) return a.valueOf() === b.valueOf();
+            if (a.toString !== Object.prototype.toString) return a.toString() === b.toString();
+
+            keys = Object.keys(a);
+            length = keys.length;
+            if (length !== Object.keys(b).length) return false;
+
+            for (i = length; i-- !== 0;)
+                if (!Object.prototype.hasOwnProperty.call(b, keys[i])) return false;
+
+            for (i = length; i-- !== 0;) {
+                var key = keys[i];
+
+                if (!equal(a[key], b[key])) return false;
+            }
+
+            return true;
+        }
+
+        // true if both NaN, false otherwise
+        return a!==a && b!==b;
+    };
+
+    /**
      * Returns a prefixed CSS class name (or selector).
      *
      * If `cls` contains spaces, each class is prefixed.
@@ -124,6 +170,12 @@ window.PhpDebugBar = window.PhpDebugBar || {};
                 for (const k in attr) {
                     this.set(k, attr[k]);
                 }
+                return;
+            }
+
+            // If data is already the same, skip
+            if (this._attributes[attr] && equal(this._attributes[attr], value)) {
+                console.log('equal', this._attributes[attr], value);
                 return;
             }
 
