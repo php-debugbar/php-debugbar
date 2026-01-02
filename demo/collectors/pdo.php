da5ccee59ead617c68097e66dcf5196b87522e14
Barry vd. Heuvel <barry@fruitcake.nl>
2026-01-02 13:54:18 +0100

    Add 2nd connection

--- demo/collectors/pdo.php
+++ demo/collectors/pdo.php
@@ -8,15 +8,17 @@ use DebugBar\DataCollector\PDO\TraceablePDO;
 /** @var \DebugBar\DebugBar $debugbar */
 
 $pdo = new TraceablePDO(new PDO('sqlite::memory:'));
-
+$pdoRead = new PDO('sqlite::memory:');
 $pdoCollector = new PDOCollector($pdo);
 $pdoCollector->enableBacktrace();
 $pdoCollector->setDurationBackground(true);
 $pdoCollector->setRenderSqlWithParams();
-
+$pdoCollector->addConnection($pdoRead, 'read');
 $debugbar->addCollector($pdoCollector);
 
 $pdo->exec('create table users (id integer, name varchar, email varchar)');
+$pdoRead->exec('create table users (id integer, name varchar, email varchar)');
+
 $stmt = $pdo->prepare('insert into users (name) values (?)');
 $stmt->execute(['foo']);
 $stmt->execute(['bar']);
@@ -27,7 +29,7 @@ $stmt->execute(['foo']);
 $stmt->execute(['foo']);
 $foo = $stmt->fetch();
 
-$stmt = $pdo->prepare('select * from users where name=:name and email=:email');
+$stmt = $pdoRead->prepare('select * from users where name=:name and email=:email');
 $stmt->execute(['name' => 'Barry', ':email' => '<script>alert();</script>']);
 $foo = $stmt->fetch();
 
