ce12b3c064daf78b4bf2370a26bbd61267fea303
Barry vd. Heuvel <barry@fruitcake.nl>
2026-01-13 09:44:58 +0100

    Add sql keywords

--- build/build-hljs.js
+++ build/build-hljs.js
@@ -20,6 +20,19 @@ hljs.registerLanguage('plaintext', plaintext);
 hljs.registerLanguage('xml', xml);
 hljs.registerLanguage('yaml', yaml);
 
+
+const sqlLang = hljs.getLanguage('sql');
+
+//Extend sql keywords
+sqlLang.keywords.keyword = Array.from(new Set([
+    ...sqlLang.keywords.keyword,
+    'if','ifnull','limit','aes_decrypt','aes_encrypt','ascii','bin','bit_and','bit_count','bit_length','bit_or','bit_xor','coercibility','concat','group_concat','concat_ws','connection_id','conv','curdate','curtime','database','date_add','date_format','date_sub','dayname','dayofmonth','dayofweek','dayofyear','elt','export_set','field','find_in_set','format','from_base64','from_days','from_unixtime','get_lock','greatest','hex','ifnull','inet_aton','inet_ntoa','instr','isnull','last_insert_id','least','lpad','ltrim','make_set','md5','monthname','now','oct','ord','password','quote','release_lock','repeat','replace','reverse','rpad','rtrim','sec_to_time','sha1','sha2','sleep','soundex','space','strcmp','str_to_date','substr','sysdate','time_format','time_to_sec','to_base64','to_days','unix_timestamp','updatexml','version','week','weekday','yearweek','length','substring_index','json_unquote','json_extract','json_contains'
+]));
+sqlLang.keywords.type = Array.from(new Set([
+    ...sqlLang.keywords.type,
+    'longtext',
+]));
+
 // Configure to use custom CSS class prefix
 hljs.configure({ classPrefix: 'phpdebugbar-hljs-' });
 
