9fee4fad594ae73981476b367df0a76dcb720c4b
Barry vd. Heuvel <barry@fruitcake.nl>
2026-01-20 19:10:42 +0100

    Add test

--- tests/Tests/Browser/DebugbarTest.php
+++ tests/Tests/Browser/DebugbarTest.php
@@ -224,4 +224,29 @@ class DebugbarTest extends AbstractBrowserTestCase
         $client->takeScreenshot(__DIR__ . '/../../screenshots/pdo.png');
 
     }
+
+    public function testIframe(): void
+    {
+        $client = static::createPantherClient();
+        $size = new WebDriverDimension(1920, 800);
+        $client->manage()->window()->setSize($size);
+
+        $client->request('GET', '/iframes/index.php');
+
+        // Wait for Debugbar to load
+        $crawler = $client->waitFor('.phpdebugbar-body');
+        usleep(1000);
+
+        if (!$this->isTabActive($crawler, 'messages')) {
+            $client->click($this->getTabLink($crawler, 'messages'));
+        }
+
+        $crawler = $client->waitForVisibility('.phpdebugbar-widgets-messages .phpdebugbar-widgets-list');
+
+        $client->waitForElementToContain('.phpdebugbar-panel[data-collector=messages]', "I'm a Deeper Hidden Iframe");
+        $client->waitForElementToContain('.phpdebugbar-widgets-datasets-badge', 'iframes/iframe2.php');
+
+        $client->takeScreenshot(__DIR__ . '/../../screenshots/iframe.png');
+
+    }
 }
