029869eafb3c3fa925e8018b472a1069e17f2f57
Caleb White <cdwhite3@pm.me>
2026-02-05 09:09:17 -0600

    fix: QueryFormatter::quoteBinding() $binding must be of type string, false given
    When the json_encode() function fails, it returns false which causes a
    TypeError downstream. Instead, we should throw an exception so that the
    actual cause can be inspected.

--- src/DataFormatter/QueryFormatter.php
+++ src/DataFormatter/QueryFormatter.php
@@ -35,7 +35,21 @@ class QueryFormatter extends DataFormatter
             }
 
             if (is_object($binding)) {
-                $binding =  json_encode($binding);
+                if ($binding instanceof \Closure) {
+                    $binding = '[CLOSURE]';
+                } else {
+                    try {
+                        $binding = json_encode(
+                            $binding,
+                            JSON_INVALID_UTF8_SUBSTITUTE
+                            | JSON_PARTIAL_OUTPUT_ON_ERROR
+                            | JSON_UNESCAPED_UNICODE
+                            | JSON_THROW_ON_ERROR
+                        );
+                    } catch (\JsonException $e) {
+                        $binding = '[OBJECT ' . get_class($binding) . ']';
+                    }
+                }
             }
         }
 
